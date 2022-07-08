ce493c6e08769cb26cd90b34a3f8d4e4dee47576
Nadir <nadirkhan98@outlook.com>
2022-07-08 15:59:17 +0100

    added animation to name

--- bundle.js
+++ bundle.js
@@ -3,6 +3,20 @@ const ind = require("./index.js");
 
 },{"./index.js":2}],2:[function(require,module,exports){
 const app = require("./app");
-document.addEventListener("DOMContentLoaded", init);
+document.addEventListener("DOMContentLoaded", () => {
+  const button = document.querySelector("#logo");
+
+  button.addEventListener("click", (e) => {
+    e.preventDefault();
+    window.scrollTo(0, 0);
+  });
+});
+
+const content = document.querySelector(".auto-type");
+var typed = new Typed(content, {
+  strings: ["Ahmed Khan"],
+  typeSpeed: 200,
+  loop: false,
+});
 
 },{"./app":1}]},{},[2]);
--- static/js/index.js
+++ static/js/index.js
@@ -1,2 +1,16 @@
 const app = require("./app");
-document.addEventListener("DOMContentLoaded", init);
+document.addEventListener("DOMContentLoaded", () => {
+  const button = document.querySelector("#logo");
+
+  button.addEventListener("click", (e) => {
+    e.preventDefault();
+    window.scrollTo(0, 0);
+  });
+});
+
+const content = document.querySelector(".auto-type");
+var typed = new Typed(content, {
+  strings: ["Ahmed Khan"],
+  typeSpeed: 200,
+  loop: false,
+});
